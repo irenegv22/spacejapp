179ce1727026c67bbba3e2d38c26df50367ea924
irenegv22 <irene.gv94@gmail.com>
2024-05-20 08:12:52 +0100

    feat: remove unsused const

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,8 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,11 +1,8 @@
 import { View, type ViewProps } from 'react-native';
 
-export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
-};
+export type ThemedViewProps = ViewProps;
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({ style, ...otherProps }: ThemedViewProps) {
   const backgroundColor = 'white';
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
