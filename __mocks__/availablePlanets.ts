8bfcbe83a639e1153bbc39309df67703a33fc37b
irenegv22 <irene.gv94@gmail.com>
2024-05-20 13:04:16 +0100

    feat: add mock planet data

---
+++ __mocks__/availablePlanets.ts
@@ -0,0 +1,22 @@
+export const planets = [
+  {
+    id: '1',
+    name: 'Luna',
+    imageUrl:
+      'https://plus.nasa.gov/wp-content/uploads/2023/08/Moon_Old-and-NewHorizontal-Hero-16_9-Without-Title.png?w=1024',
+    accessibilityLabel: 'Moon image',
+  },
+  {
+    id: '2',
+    name: 'Marte',
+    imageUrl:
+      'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2007/02/true-colour_image_of_mars_seen_by_osiris/9969705-2-eng-GB/True-colour_image_of_Mars_seen_by_OSIRIS_pillars.jpg',
+    accessibilityLabel: 'Mars image',
+  },
+  {
+    id: '3',
+    name: 'Estación Intern.',
+    imageUrl: 'https://www.nasa.gov/wp-content/uploads/2023/02/International-Space-Station-in-2021.jpg',
+    accessibilityLabel: 'International Station image',
+  },
+];
