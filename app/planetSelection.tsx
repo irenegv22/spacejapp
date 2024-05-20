00dc17f384c002f1230c29f687ed1fdc7385ae7b
irenegv22 <irene.gv94@gmail.com>
2024-05-20 13:06:25 +0100

    feat: add bookingInfo type and start form

--- app/planetSelection.tsx
+++ app/planetSelection.tsx
@@ -1,15 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { type FC } from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import { useState, type FC } from 'react';
 import CustomHeader from '@/components/CustomHeader/CustomHeader';
 import globalStyles, { horizontalPadding } from '@/constants/GlobalStyles';
+import Colors from '@/constants/Colors';
+import typos from '@/constants/Typos';
+import { BookingInfo } from '@/types/bookingInfo';
+import PlanetCard from '@/components/PlanetCard/PlanetCard';
+import { planets } from '@/__mocks__/availablePlanets';
+
+const formInitialValue: BookingInfo = {
+  id: '',
+  planet: '',
+  travelerName: '',
+  travelerSurame: '',
+  travelerPhone: '',
+  // bookingDate: '',
+  // tripDate: '',
+  // tripHour: '',
+  menuChoice: '',
+  specialRequest: '',
+};
 
 const PlanetSelectionScreen: FC = () => {
+  const [formData, setFormData] = useState<BookingInfo>(formInitialValue);
+  const [selectedPlanetId, setSelectedPlanetId] = useState<string | null>(null);
+
+  const handleChange = (name: keyof BookingInfo, value: string) => {
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectPlanet = (id: string) => {
+    setSelectedPlanetId(id);
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <CustomHeader title={'Seleciona un planeta'} back />
-      <View style={styles.contentContainer}>
-        <Text>Elije el planeta al que quieras viajar</Text>
-      </View>
+      <CustomHeader title={'Nuevo Viaje'} back />
+      <ScrollView style={styles.contentContainer}>
+        <Text style={styles.contentTitle}>Selecciona tu destino</Text>
+        <View style={styles.cardsContainer}>
+          {planets.map(planet => (
+            <PlanetCard
+              key={planet.id}
+              name={planet.name}
+              imageUrl={planet.imageUrl}
+              onSelect={() => handleSelectPlanet(planet.id)}
+              selected={planet.id === selectedPlanetId}
+              accessibilityLabel={planet.accessibilityLabel}
+            />
+          ))}
+        </View>
+        <View style={styles.formContainer}>
+          <View style={styles.formGroup}>
+            <TextInput
+              placeholder="Nombre"
+              style={styles.formInputText}
+              keyboardType="default"
+              value={formData.travelerName}
+              onChange={event => handleChange('travelerName', event.nativeEvent.text)}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <TextInput
+              placeholder="Apellido"
+              style={styles.formInputText}
+              keyboardType="default"
+              value={formData.travelerSurame}
+              onChange={event => handleChange('travelerSurame', event.nativeEvent.text)}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <TextInput
+              placeholder="Teléfono"
+              style={styles.formInputText}
+              keyboardType="number-pad"
+              value={formData.travelerPhone}
+              onChange={event => handleChange('travelerPhone', event.nativeEvent.text)}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -21,6 +94,29 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingHorizontal: horizontalPadding,
+  },
+  formContainer: {
+    paddingVertical: 10,
+    display: 'flex',
+  },
+  contentTitle: {
+    ...typos.bodyOne,
+  },
+  cardsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  formGroup: {
+    marginBottom: 10,
+    marginHorizontal: 0,
+  },
+  formInputText: {
+    height: 54,
+    backgroundColor: Colors.Grey150,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    ...typos.bodyOne,
   },
 });
 
---
+++ types/bookingInfo.ts
@@ -0,0 +1,12 @@
+export interface BookingInfo {
+  id: string;
+  planet: string;
+  travelerName: string;
+  travelerSurame: string;
+  travelerPhone: string;
+  //   bookingDate: Date;
+  //   tripDate: Date;
+  //   tripHour: Date;
+  menuChoice: string;
+  specialRequest?: string;
+}
