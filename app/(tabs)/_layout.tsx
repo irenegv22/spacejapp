b1607eab17e19960a7ad0752898da77eee1f1efc
irenegv22 <irene.gv94@gmail.com>
2024-05-23 22:12:45 +0100

    feat: modify tabBar focus color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,36 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import Colors from '@/constants/Colors';
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: Colors.Main100,
+        tabBarInactiveTintColor: Colors.Grey250,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Inicio',
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={focused ? Colors.Main100 : Colors.Grey250} />
+          ),
         }}
       />
       <Tabs.Screen
         name="bookings"
         options={{
           title: 'Mis viajes',
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'planet' : 'planet-outline'} color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name={focused ? 'planet' : 'planet-outline'}
+              color={focused ? Colors.Main100 : Colors.Grey250}
+            />
+          ),
         }}
       />
     </Tabs>
